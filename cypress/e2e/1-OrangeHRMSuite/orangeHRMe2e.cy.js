a6c6e727d46c7a11df5e680dccf34099ae0a3b50
rezaparamarta <rezaparamarta30@gmail.com>
2025-05-12 00:52:03 +0700

    Add Negative Case

--- cypress/e2e/1-OrangeHRMSuite/orangeHRMe2e.cy.js
+++ cypress/e2e/1-OrangeHRMSuite/orangeHRMe2e.cy.js
@@ -15,7 +15,7 @@ describe('Add New Employee', function () {
     });
   });
 
-  it('Login and Add New Employee', function () {
+  it('Soal ke 1 - Positive Case - Login and Add New Employee', function () {
     cy.get(':nth-child(2) > .oxd-main-menu-item').click();
     cy.url().should('include', '/web/index.php/pim/viewEmployeeList');
     cy.wait(1000);
@@ -82,20 +82,166 @@ describe('Add New Employee', function () {
       cy.url().should('contains', '/web/index.php/pim/viewPersonalDetails/empNumber/');
   });
 
-  // Test Case Soal 2
-  it.only('Login and Add Jatah Cuti', function() {
+  it('Soal ke 1 - Negative Case - Login and Fail Add New Employee', function () {
+    cy.get(':nth-child(2) > .oxd-main-menu-item').click();
+    cy.url().should('include', '/web/index.php/pim/viewEmployeeList');
+    cy.wait(1000);
+    // Click (+Add) button
+    cy.xpath("//button[normalize-space()='Add']").click();
+    cy.url().should('include', '/web/index.php/pim/addEmployee');
+    // Upload file
+    cy.xpath("//input[@type='file']")
+      .should('exist')
+      .selectFile("C:\\Users\\Reza Paramarta\\photo.jpg", { force: true });
+    // Verify file upload
+    cy.xpath("//input[@type='file']")
+      .invoke('val')
+      .should('include', 'photo.jpg');
+    // Input data
+    cy.xpath("//input[@placeholder='First Name']").type("Reza");
+    cy.xpath("//input[@placeholder='Middle Name']").clear();
+    cy.xpath("//input[@placeholder='Last Name']").type("");
+    // Verify input data
+    cy.xpath("//input[@placeholder='First Name']").should("have.value", "Reza");
+    cy.xpath("//input[@placeholder='Middle Name']")
+      .invoke("val")
+      .then(val => expect(val.trim()).to.eq(""));
+    cy.xpath("//input[@placeholder='Last Name']").should("have.value", "Paramarta");
+    // Input dan Get Id employee
+    cy.xpath("//div[@class='oxd-input-group oxd-input-field-bottom-space']//div//input[@class='oxd-input oxd-input--active']")
+      .invoke('val')
+      .should('not.be.empty')
+      .then((value) => {
+        cy.log('Employee ID:', value);
+    // Verify input dan Get Id employee
+        cy.request({
+          method: 'GET',
+          url: '/web/index.php/api/v2/core/validation/unique',
+          qs: {
+            value: value,
+            entityName: 'Employee',
+            attributeName: 'employeeId'
+          },
+          failOnStatusCode: false
+        }).then((res) => {
+          if (!res.body.data.valid) {
+            const newId = 'EMP' + Date.now().toString().slice(-5);
+            cy.xpath("//div[@class='oxd-input-group oxd-input-field-bottom-space']//div//input[@class='oxd-input oxd-input--active']")
+              .clear()
+              .type(newId)
+              .should('have.value', newId);
+            cy.log('Updated ID:', newId);
+          }
+
+          cy.intercept('POST', '**/pim/employees').as('createEmployee');
+          cy.xpath("//button[normalize-space()='Save']").click();
+
+          cy.wait('@createEmployee').then((interception) => {
+            cy.log('Request:', JSON.stringify(interception.request.body));
+            cy.log('Status:', interception.response.statusCode);
+            cy.log('Response:', JSON.stringify(interception.response.body));
+
+            expect(interception.response.statusCode).to.eq(200);
+          });
+        });
+      });
+      // verify success add employee
+      cy.url().should('contains', '/web/index.php/pim/viewPersonalDetails/empNumber/');
+  });
+
+  // Test Case Soal 2 -- Positive Case
+  it('Soal ke - 2 : Positive Case - Login and Add Jatah Cuti', function() {
     // Ensure Leave and Click its module
     cy.xpath("//a[@class='oxd-main-menu-item active']").should('be.visible');
     cy.get(':nth-child(3) > .oxd-main-menu-item').click();
-  
+
     // Click Entitlements
     cy.get('.oxd-topbar-body-nav > ul > :nth-child(3)').click();
     cy.xpath("//a[normalize-space()='Add Entitlements']").click();
 
     // Type on Employee Name*
-    cy.xpath("//div[@class='oxd-autocomplete-text-input oxd-autocomplete-text-input--active']").type("akhil");
-    cy.wait(3000);
-    cy.xpath("//div[contains(@class, 'oxd-autocomplete-text-input')]//div[@class='oxd-autocomplete-dropdown-menu']//li[normalize-space()='Akhil Akbar']").click();
+    cy.xpath("//input[@placeholder='Type for hints...']").type('Peter');
 
-  });
+    // Waiting on API response
+    cy.get('.oxd-autocomplete-dropdown > div')
+      .contains('Peter Mac Anderson')
+      .click();
+
+    // Verify Field filled with Peter Mac Anderson
+    cy.xpath("//input[@placeholder='Type for hints...']").should('have.value', 'Peter Mac Anderson');
+
+    // Klik dropdown-nya
+    cy.xpath("//label[text()='Leave Type']/ancestor::div[contains(@class,'oxd-input-group')]//div[contains(@class,'oxd-select-text')]").click({multiple: true});
+    cy.get('.oxd-select-dropdown > div').eq(2).click();
+
+    // Verify filled with index ke 2
+    cy.xpath("//label[text()='Leave Type']/ancestor::div[contains(@class,'oxd-input-group')]//div[contains(@class,'oxd-select-text')]").should('have.text', 'CAN - BereavementCAN - Bereavement');
+
+    // Click Leave Period*
+    cy.xpath("//div[contains(text(),'2025-01-01 - 2025-31-12')]").click({multiple: true});
+    // Verify Leave Period filled with 2025-01-01 - 2025-31-12
+    cy.xpath("//div[contains(text(),'2025-01-01 - 2025-31-12')]").should('have.text', '2025-01-01 - 2025-31-12');
+
+    // Type on Entitlement*
+    let Entitlement = 2025;
+    cy.xpath("//div[@class='oxd-input-group oxd-input-field-bottom-space']//div//input[@class='oxd-input oxd-input--active']").type(Entitlement);
+
+    // Click 'Save' Button
+    cy.xpath("//button[normalize-space()='Save']").click();
+
+    // Popup Confirm
+    cy.get('.oxd-sheet').should('be.visible');
+
+    // Click 'Confirm' Button
+    cy.get('.orangehrm-modal-footer > .oxd-button--secondary').click();
+
+    // Verify Entitlements Success
+    cy.url().should('include', '/web/index.php/leave/viewLeaveEntitlements');
+
+    });
+
+  // Test Case Soal 2 -- Negative Case
+  it('Soal ke 2 : Negative Case - Fail Add Jatah Cuti', function() {
+    // Ensure Leave and Click its module
+    cy.xpath("//a[@class='oxd-main-menu-item active']").should('be.visible');
+    cy.get(':nth-child(3) > .oxd-main-menu-item').click();
+
+    // Click Entitlements
+    cy.get('.oxd-topbar-body-nav > ul > :nth-child(3)').click();
+    cy.xpath("//a[normalize-space()='Add Entitlements']").click();
+
+    // Type on Employee Name*
+    cy.xpath("//input[@placeholder='Type for hints...']").type('Peter');
+
+    // Waiting on API response
+    cy.get('.oxd-autocomplete-dropdown > div')
+      .contains('Peter Mac Anderson')
+      .click();
+
+    // Verify Field filled with Peter Mac Anderson
+    cy.xpath("//input[@placeholder='Type for hints...']").should('have.value', 'Peter Mac Anderson');
+
+    // Klik dropdown-nya
+    cy.xpath("//label[text()='Leave Type']/ancestor::div[contains(@class,'oxd-input-group')]//div[contains(@class,'oxd-select-text')]").click({multiple: true});
+    cy.get('.oxd-select-dropdown > div').eq(2).click();
+
+    // Verify filled with index ke 2
+    cy.xpath("//label[text()='Leave Type']/ancestor::div[contains(@class,'oxd-input-group')]//div[contains(@class,'oxd-select-text')]").should('have.text', 'CAN - BereavementCAN - Bereavement');
+
+    // Click Leave Period*
+    cy.xpath("//div[contains(text(),'2025-01-01 - 2025-31-12')]").click({multiple: true});
+    // Verify Leave Period filled with 2025-01-01 - 2025-31-12
+    cy.xpath("//div[contains(text(),'2025-01-01 - 2025-31-12')]").should('have.text', '2025-01-01 - 2025-31-12');
+
+    // Type on Entitlement*
+    let Entitlement = 'Ablahulah';
+    cy.xpath("//div[@class='oxd-input-group oxd-input-field-bottom-space']//div//input[@class='oxd-input oxd-input--active']").type(Entitlement);
+
+    // Verify Negative Test Case confirmed
+    cy.get('.oxd-input-group > .oxd-text').should('have.text', 'Should be a number with upto 2 decimal places');
+
+    // Click 'Save' Button
+    cy.xpath("//button[normalize-space()='Save']").click();
+
+    });
 });
