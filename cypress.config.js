c2174ce589e341dd77d50517fabc6b11728e2830
rezaparamarta <rezaparamarta30@gmail.com>
2025-05-12 11:07:08 +0700

    Change cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: false,
+    json: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // Optional: tambahkan listener Node.js kalau diperlukan
